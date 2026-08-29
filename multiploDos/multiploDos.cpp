c28a388010eb7c56cd672d152b16710b550ce433
4tsc <tomascheel@outlook.com>
2026-08-29 19:15:54 -0400

    LITERAL COMO MULTIPLICAR

---
+++ multiploDos/multiploDos.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+	int a {};
+
+	std::cout << "VAMOS A MULTIPLICAR POR DOS \n" << "Ingresa tu numero: " << '\n';
+
+	std::cin >> a;
+
+	std::cout << "EL NUMERO DUPLICADO ES: " << (a * 2) << '\n';	
+
+	return 0;
+}
