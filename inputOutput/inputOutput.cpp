dd81f7d305d379912ffd0c4e5d69ec75a82fb526
4tsc <tomascheel@outlook.com>
2026-08-29 17:37:04 -0400

    cout y cin

---
+++ inputOutput/inputOutput.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+	int a {};	
+	int b {};
+
+
+	std::cout << "Ingresa dos numeros lol \n";
+
+	std::cin >> a >> b;
+
+	std::cout << "primer wea: " << a << " segunda wea: " << b << '\n';	
+
+	return 0;
+}
